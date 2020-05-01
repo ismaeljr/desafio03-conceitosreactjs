3d43d0b0f98e3ded59f1b195196234e99d0fc64b
Ismael Skywalker <ismael_junior@msn.com>
2020-04-30 23:14:46 -0300

    Desafio Conceitos ReactJs
    Neste desafio tive que criar uma função para add e deletar componetes da aplicação, assim como utilizar o conceito de estado e imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from "react";
-
+import React, {useState , useEffect} from "react";
+const { uuid } = require("uuidv4");
 import "./styles.css";
+
+import api from "./services/api";
+
+/*
+id: repositories[repIndex].id,
+      title,
+      url,
+      techs,
+      likes
+*/
 
 function App() {
   async function handleAddRepository() {
-    // TODO
+    // função para adicionar repositorio
+    const response = await api.post('repositories',{
+      id: uuid(),
+      title: `Projeto ${Date.now()}`,
+      url: "http://www.google.com.br",
+      techs: ["ReactJS","NodeJS","Power BI"],
+    });
+
+    const newRepository = response.data;
+
+    setRepository([...repositories,newRepository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    // função para deletar
+    const base_url = 'repositories/'+id;
+
+    const response = api.delete(base_url);    
+    
+    setRepository (
+      repositories.filter((repository) => repository.id !== id)
+    );
+
   }
 
+  
+
+
+  const [repositories, setRepository] = useState([]);
+
+  useEffect(() =>{
+    api.get('repositories').then(response => {
+      setRepository(response.data);
+      console.log(response);
+    });
+
+  },[]);
+
+
+
   return (
+    
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        
+          {repositories.map(repositories => <li key={repositories.id}>{repositories.title}
+          <button onClick={() => handleRemoveRepository(repositories.id)}>
             Remover
-          </button>
-        </li>
+          </button>          
+        </li>)}
+
+          
+        
+
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
